9fd964c309d01f4a8fcf8034944d5dbafeadb69b
desetti praneeth <praneethdesetti@gmail.com>
2023-11-14 13:38:55 +0530

    v1

--- LMS-BE/src/main/java/com/lms/config/SecurityConfig.java
+++ LMS-BE/src/main/java/com/lms/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -27,7 +32,7 @@ public class SecurityConfig {
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
 				List<String> listoforigin = List.of("http://localhost:3000/");
-				List<String> listofmethods = List.of("GET", "POST","PUT","DELETE");
+				List<String> listofmethods = List.of("GET", "POST", "PUT", "DELETE");
 				CorsConfiguration cfg = new CorsConfiguration();
 
 				cfg.setAllowedOrigins(listoforigin);
@@ -43,6 +48,19 @@ public class SecurityConfig {
 		http.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
 
 		return http.build();
+	}
+
+	@Bean
+	InMemoryUserDetailsManager imuds() {
+
+		UserDetails user1 = User.withUsername("spring").password(pe().encode("123")).build();
+
+		return new InMemoryUserDetailsManager(user1);
+	}
+
+	@Bean
+	PasswordEncoder pe() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
