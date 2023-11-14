ce126726535a79fa23c34f7ddc9ffc8fa7067acc
desetti praneeth <praneethdesetti@gmail.com>
2023-11-14 13:05:15 +0530

    v1

--- LMS-BE/src/main/java/com/lms/controller/LearnerUserController.java
+++ LMS-BE/src/main/java/com/lms/controller/LearnerUserController.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lms.entity.LearnerUser;
+import com.lms.exception.DbException;
 
 @RestController
 @RequestMapping("/user")
@@ -21,9 +22,6 @@ public class LearnerUserController {
 
 	@Autowired
 	private DataSource ds;
-
-	@Autowired
-	private Environment environment;
 
 	@PostMapping("/save")
 	public LearnerUser save(@RequestBody LearnerUser lu) {
@@ -33,19 +31,12 @@ public class LearnerUserController {
 	}
 
 	@GetMapping("/testdb")
-	public String test() throws SQLException {
+	public ResponseEntity<String> testdb() throws SQLException {
 
-		String dbUsername = environment.getProperty("spring.datasource.username");
-		String dbPassword = environment.getProperty("spring.datasource.password");
-
-		Connection connection = ds.getConnection();
-		Connection connection2 = ds.getConnection(dbUsername, dbPassword);
-		
-		
-		if (connection2 != null) {
-			return "Api Is Connected " + dbUsername;
-		} else {
-			return "Api Not Working";
+		try (Connection c = ds.getConnection()) {
+			throw new DbException("Db Not Connected");
+		} catch (Exception e) {
+			return ResponseEntity.ok("Db Connected");
 		}
 
 	}
