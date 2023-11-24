7e53f50504013e1d27d1fd9a7721bef228341989
tejagva <118526336+tejagva@users.noreply.github.com>
2023-11-24 19:09:18 +0530

    Update Home.jsx

--- lms-fe/lmsv1/src/Components/Home.jsx
+++ lms-fe/lmsv1/src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   const handleButtonClick = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/user/login");
+      const response = await axios.get("http://3.110.195.242:8080/user/login");
       console.log(response);
       setMessage(response.data);
     } catch (error) {
